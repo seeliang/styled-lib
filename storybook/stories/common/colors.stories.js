2cc32440d52af8745e061a075b9f82e7709a4983
seeliang <me@sihuiliang.com>
2019-06-09 12:03:04 +1000

    feat: generate color shortcut

--- storybook/stories/common/colors.stories.js
+++ storybook/stories/common/colors.stories.js
@@ -4,6 +4,17 @@ import { storiesOf } from '@storybook/react';
 
 import common from '../../../common';
 
+
+const generateColor = common => {
+    const color = {}
+    Object.keys(common.colors).forEach(i => {
+        color[i] = props => props.theme.colors[i]
+    });
+    return color;
+}
+
+const color = generateColor(common);
+
 const Section = styled.div`
     display: inline-block;
     float: left;
@@ -14,12 +25,12 @@ const Section = styled.div`
 const ColorBlock = styled.div`
     width: 60px;
     height: 60px;
-    background: ${props => props.theme.colors[props.color]};
+    background: ${props => color[props.color]};
 `
 const AllColors = () => {
     const colorsList = Object.keys(common.colors);
     return colorsList.map(i => 
-        <Section> 
+        <Section key={i}> 
             <h4> {i} </h4> 
             <ColorBlock color={i} />
         </Section>
