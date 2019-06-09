36943504fb9f86b950dd699d8a9ac167e8e455e5
seeliang <me@sihuiliang.com>
2019-06-09 11:14:09 +1000

    feat: listing all colors

--- storybook/stories/common/colors.stories.js
+++ storybook/stories/common/colors.stories.js
@@ -2,13 +2,33 @@ import React from 'react';
 import styled from 'styled-components'
 import { storiesOf } from '@storybook/react';
 
+import common from '../../../common';
+
+const Section = styled.div`
+    display: inline-block;
+    float: left;
+    margin: 10px;
+    min-width: 120px;
+`
+
 const ColorBlock = styled.div`
     width: 60px;
     height: 60px;
-    background: ${props => props.theme.colors.primary};
+    background: ${props => props.theme.colors[props.color]};
 `
+const AllColors = () => {
+    const colorsList = Object.keys(common.colors);
+    return colorsList.map(i => 
+        <Section> 
+            <h4> {i} </h4> 
+            <ColorBlock color={i} />
+        </Section>
+    );
+}
 
 storiesOf('Common.Colors', module).add('Default', () => 
-    <ColorBlock />
+   
+    <AllColors />
+
 );
 
