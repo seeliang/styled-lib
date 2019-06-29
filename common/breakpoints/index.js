c241b9db43f4bd50caadd937f9cd2c6c23f1db41
seeliang <me@sihuiliang.com>
2019-06-29 21:30:33 +1000

    feat: better scope

--- common/breakpoints/index.js
+++ common/breakpoints/index.js
@@ -1,9 +1,11 @@
+
+const maxPhone = 768;
 const maxTablet = 1280;
 const maxDesktop = 1920;
 
 export const breakpoints = {
   min: 320,
-  maxPhone: 768,
+  maxPhone,
   maxTablet,
   maxDesktop,
   maxMobile: maxTablet,
