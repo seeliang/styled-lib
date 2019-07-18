49c3e09df284b1a7de4d943e848bbf263f21b822
seeliang <me@sihuiliang.com>
2019-07-19 09:02:15 +1000

    feat: add tablet col

--- storybook/stories/partials/grid.stories.js
+++ storybook/stories/partials/grid.stories.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 import { storiesOf } from '@storybook/react';
 
 import { screen, color } from '../../helpers';
@@ -9,6 +9,7 @@ const totalCol = {
   desktop: 12,
   tablet: 4,
   phone: 2,
+  mobile: 2,
 };
 
 const Block = styled.p`
@@ -23,9 +24,11 @@ const getBreakpoints = set => props => `${props.theme.breakpoints[set]}px`;
 
 const ColorBlock = ({ text }) => <Block>{text}</Block>;
 
-const getWidth = ({ props, col }) => {
-  const { desktop } = props;
-  const cut = desktop || col;
+const getWidth = ({ num, col }) => {
+  const cut = num || col;
+  if (cut < 1 || cut > num) {
+    throw new Error(`your col number should be in between of 1 and ${col}`);
+  }
   const percent = 100 / col * cut;
   return `${percent}%`;
 };
@@ -38,77 +41,85 @@ const Row = styled.div`
   max-width: ${getBreakpoints('max')};
 `;
 
+const generateScreenWidth = set => css`
+ ${screen(set)} {
+    width: ${props => getWidth({ num: props[set], col: totalCol[set] })};
+  }
+`;
+
 const Col = styled.div`
   padding: 0 ${gutter / 2}px;
   display: inline-block;
-  ${screen('desktop')} {
-    width: ${props => getWidth({ props, col: totalCol.desktop })};
-  }
+  ${generateScreenWidth('desktop')};
+  ${generateScreenWidth('tablet')};
 `;
 
 storiesOf('Partials.grid', module).add('Default', () => (
   <>
+    <p>
+      {`totalCol: ${JSON.stringify(totalCol)}`}
+    </p>
     <Row>
       <Col desktop={1}>
-        <ColorBlock text="1" />
+        <ColorBlock text="desktop: 1" />
       </Col>
       <Col desktop={2}>
-        <ColorBlock text="2" />
+        <ColorBlock text="desktop: 2" />
       </Col>
       <Col desktop={3}>
-        <ColorBlock text="3" />
+        <ColorBlock text="desktop: 3" />
       </Col>
 
       <Col desktop={6}>
-        <ColorBlock text="6" />
+        <ColorBlock text="desktop: 6" />
       </Col>
     </Row>
     <Row>
-      <Col desktop={7}>
-        <ColorBlock text="7" />
+      <Col desktop={7} tablet={1}>
+        <ColorBlock text="desktop: 7, tablet: 1" />
       </Col>
-      <Col desktop={5}>
-        <ColorBlock text="5" />
+      <Col desktop={5} tablet={3}>
+        <ColorBlock text="desktop: 5, tablet: 3" />
       </Col>
     </Row>
 
     <Row>
-      <Col desktop={8}>
-        <ColorBlock text="8" />
+      <Col desktop={8} tablet={2}>
+        <ColorBlock text="desktop: 8, tablet 2" />
       </Col>
-      <Col desktop={4}>
-        <ColorBlock text="4" />
+      <Col desktop={4} tablet={2}>
+        <ColorBlock text="desktop: 4, tablet 2" />
       </Col>
     </Row>
 
     <Row>
       <Col desktop={9}>
-        <ColorBlock text="9" />
+        <ColorBlock text="desktop: 9" />
       </Col>
       <Col desktop={3}>
-        <ColorBlock text="3" />
+        <ColorBlock text="desktop: 3" />
       </Col>
     </Row>
 
     <Row>
       <Col desktop={10}>
-        <ColorBlock text="10" />
+        <ColorBlock text="desktop: 10" />
       </Col>
       <Col desktop={2}>
-        <ColorBlock text="2" />
+        <ColorBlock text="desktop: 2" />
       </Col>
     </Row>
     <Row>
       <Col desktop={11}>
-        <ColorBlock text="11" />
+        <ColorBlock text="desktop: 11" />
       </Col>
       <Col desktop={1}>
-        <ColorBlock text="1" />
+        <ColorBlock text="desktop: 1" />
       </Col>
     </Row>
     <Row>
       <Col>
-        <ColorBlock text="12" />
+        <ColorBlock text="" />
       </Col>
     </Row>
   </>
