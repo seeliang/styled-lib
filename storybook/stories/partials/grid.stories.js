e54da3f8ba606e833f84ff6077ba9d2dbafa1e6b
seeliang <me@sihuiliang.com>
2019-07-18 17:27:04 +1000

    feat: initial col for all media

--- storybook/stories/partials/grid.stories.js
+++ storybook/stories/partials/grid.stories.js
@@ -5,6 +5,7 @@ import { storiesOf } from '@storybook/react';
 import { screen, color } from '../../helpers';
 
 const gutter = 24;
+const totalCol = 12;
 
 const Block = styled.p`
   margin-top: 20px;
@@ -18,6 +19,13 @@ const getBreakpoints = set => props => `${props.theme.breakpoints[set]}px`;
 
 const ColorBlock = ({ text }) => <Block>{text}</Block>;
 
+const getWidth = (props) => {
+  const { initial } = props;
+  const cut = initial || 12;
+  const percent = 100 / totalCol * cut;
+  return `${percent}%`;
+};
+
 const Row = styled.div`
   padding: 0 ${gutter / 2}px;
   display: block;
@@ -28,13 +36,71 @@ const Row = styled.div`
 
 const Col = styled.div`
   padding: 0 ${gutter / 2}px;
+  display: inline-block;
+  width: ${props => getWidth(props)};
 `;
 
 storiesOf('Partials.grid', module).add('Default', () => (
-  <Row>
-    <Col>
-      <ColorBlock text={12} />
-    </Col>
-  </Row>
+  <>
+    <Row>
+      <Col>
+        <ColorBlock text="12" />
+      </Col>
+    </Row>
+
+
+    <Row>
+      <Col initial={6}>
+        <ColorBlock text="6" />
+      </Col>
+      <Col initial={6}>
+        <ColorBlock text="6" />
+      </Col>
+    </Row>
+
+    <Row>
+      <Col initial={7}>
+        <ColorBlock text="7" />
+      </Col>
+      <Col initial={5}>
+        <ColorBlock text="5" />
+      </Col>
+    </Row>
+
+    <Row>
+      <Col initial={8}>
+        <ColorBlock text="8" />
+      </Col>
+      <Col initial={4}>
+        <ColorBlock text="4" />
+      </Col>
+    </Row>
+
+    <Row>
+      <Col initial={9}>
+        <ColorBlock text="9" />
+      </Col>
+      <Col initial={3}>
+        <ColorBlock text="3" />
+      </Col>
+    </Row>
+
+    <Row>
+      <Col initial={10}>
+        <ColorBlock text="10" />
+      </Col>
+      <Col initial={2}>
+        <ColorBlock text="2" />
+      </Col>
+    </Row>
+    <Row>
+      <Col initial={11}>
+        <ColorBlock text="11" />
+      </Col>
+      <Col initial={1}>
+        <ColorBlock text="1" />
+      </Col>
+    </Row>
+  </>
 ));
 /* eslint-enable */
